1a69e931dde8e410cae86794fe7a9fed6c8e930b
BiniyamMelaku2 <BiniyamMelaku2@gmail.com>
2021-06-14 17:45:25 +0000

    implements Selection sort algorithm

---
+++ 2-selection_sort.c
@@ -0,0 +1,35 @@
+#include "sort.h"
+
+/**
+ * selection_sort - sorts in ascending order using selection sort
+ * @array: an array of integers
+ * @size: max size of the array
+ * Return: Always 0
+ */
+void selection_sort(int *array, size_t size)
+{
+int min, tmp;
+size_t i, j = 0, index_min;
+if (!array || size <= 0)
+return;
+else
+for (j = 0; j < size; j++)
+{
+min = array[j];
+for (i = j; i < size; i++)
+{
+if (array[i] < min)
+{
+min = array[i];
+index_min = i;
+}
+}
+if (index_min > j)
+{
+tmp = array[j];
+array[j] = min;
+array[index_min] = tmp;
+print_array(array, size);
+}
+}
+}
